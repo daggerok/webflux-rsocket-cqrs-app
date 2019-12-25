5789818974b5cfb118d2172c5725f2d8699a47f8
Maksim Kostromin <daggerok@gmail.com>
2019-12-25 04:44:24 +0200

    Delete V0__001_ddl.sql

--- users/src/main/resources/db/migration/V0__001_ddl.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS users;
-CREATE TABLE users(
-    id       UUID    NOT NULL,
-    name     VARCHAR NOT NULL,
-    username VARCHAR NOT NULL,
-    CONSTRAINT users_pk PRIMARY KEY ( id )
-);
