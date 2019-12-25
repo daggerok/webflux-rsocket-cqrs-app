8c6e61e646a9bdbc66295e572cdb6b93d0febd67
Maksim Kostromin <daggerok@gmail.com>
2019-12-25 04:44:36 +0200

    Delete V0__002_dml.sql

--- users/src/main/resources/db/migration/V0__002_dml.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO users (id, name, username)
-VALUES   ('00000000-0000-0000-0000-000000000001', 'Maksim Kostromin', 'daggerok')
-        ,('00000000-0000-0000-0000-000000000002', 'Maksimko'        , 'maksimko')
-        ;
